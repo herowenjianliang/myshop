5f6fc232df2bd00118ba882e964a591f70cb0955
1961424442 <1961424442@qq.com>
2019-06-04 16:02:06 +0800

    myshop

---
+++ app/Http/Controllers/AdminController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    public function index()
+    {
+        echo 1111111;
+    }
+
+
+    public function add()
+    {
+        echo 12222222111;
+    }
+}
